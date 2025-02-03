a63a582e21e83525b3976a7e29c5691a85eb1fc2
The-Foible <xander.doom@gmail.com>
2025-02-03 10:53:38 -0500

    Project Structuring

---
+++ webapp/vite.config.ts
@@ -0,0 +1,21 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vitest/config";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 2825,
+    // WSL2 doesn't propogate file changes, so we need to poll to enable HMR
+    watch: {
+      usePolling: true,
+      interval: 500,
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+});
