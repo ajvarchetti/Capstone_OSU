65c442a920246bd5a6ac2b1d87648228418f5f54
The-Foible <xander.doom@gmail.com>
2025-03-19 17:47:11 -0400

    Text changes

--- db/database_import.py
+++ db/database_import.py
@@ -5,10 +5,14 @@ import os
 from elasticsearch import Elasticsearch, helpers
 
 # Configure Elasticsearch
-ES_HOST = os.getenv("ES_HOST", "http://elasticsearch:9200")
+ES_HOST = os.getenv("ES_HOST")
+if not ES_HOST:
+    raise EnvironmentError("The environment variable 'ES_HOST' is not set. Do you have a .env file?")
+
 INDEX_NAME = "wikipedia_conspiracies"
 WIKI_API_URL = "https://en.wikipedia.org/api/rest_v1/page/summary/"
-DATA_FILE = "Data/cleaned_2024Data.json"  # Update data file path
+DATA_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+DATA_FILE = os.path.join(DATA_FOLDER, "wiki_articles.json")
 # Wait for Elasticsearch to start
 def wait_for_es():
     while True:
@@ -96,5 +100,6 @@ def import_data():
         # 移除错误详情的打印
 
 if __name__ == "__main__":
+    print("Starting data import...")
     wait_for_es()  # Ensure Elasticsearch is running
     import_data()  # Import JSON data
